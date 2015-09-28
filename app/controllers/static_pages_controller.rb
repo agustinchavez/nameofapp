a73bb9dfbcca95adcfa93e92a7b4eb0b90884c41
agustinchavez <chavez13@illinois.edu>
2015-09-28 10:59:02 -0500

    exercise35 changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,10 @@
 class StaticPagesController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def landing_page
-    @featured_product = Product.first
+    @featured_product = Product.all
     @products = Product.limit(3)
   end
 end
