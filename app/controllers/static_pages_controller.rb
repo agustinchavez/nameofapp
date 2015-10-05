80b5460053749b9b3bf366bc7e3a1465db6c8524
agustinchavez <chavez13@illinois.edu>
2015-10-05 10:58:08 -0500

    exercise 38

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,7 +8,7 @@ class StaticPagesController < ApplicationController
     @products = Product.limit(3)
   end
 
-  ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver
+  ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'chavez.agustin@outlook.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver
 
 def thank_you
   @name = params[:name]
