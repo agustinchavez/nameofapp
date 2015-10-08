9a930f22076ef79a66f925bff1a39b092a195cc3
agustinchavez <chavez13@illinois.edu>
2015-10-08 11:31:13 -0500

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007120209) do
-
-  create_table "comments", force: :cascade do |t|
-    t.integer  "user_id"
-    t.text     "body"
-    t.integer  "rating"
-    t.integer  "product_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comments", ["product_id"], name: "index_comments_on_product_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+ActiveRecord::Schema.define(version: 20151007163758) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -38,29 +26,26 @@ ActiveRecord::Schema.define(version: 20151007120209) do
     t.string   "name"
     t.text     "description"
     t.string   "image_url"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "color"
-    t.decimal  "price",       precision: 8, scale: 2
-    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "colour"
   end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.boolean  "admin",                  default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
