e9d56260309149bf65e717b52c24a619f0c3a9e1
agustinchavez <chavez13@illinois.edu>
2015-10-09 10:40:47 -0500

    removed unnecessary OrdesController code

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,31 +1,22 @@
 class OrdersController < ApplicationController
-  # before_filter :authorize_admin, only: :index
-  load_and_authorize_resource
 
   def index
-    @orders = Order.all
+
   end
 
   def show
   end
 
   def new
-    @order = Order.new
+
   end
 
   def create
-    @order = Order.new(order_params)
+
   end
 
   # def authorize_admin
   #   redirect_to :back, :status => 401 unless current_user.admin
   #   #redirects to previous page
   # end
-
-  private
-
-    def order_params
-     params.require(:order).permit(:user_id, :product_id, :total)
-    end
-
 end
