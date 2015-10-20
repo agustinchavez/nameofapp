8ae1e1d343773da8936ef736394ba35562a84830
agustinchavez <chavez13@illinois.edu>
2015-10-20 14:01:08 -0500

    edited comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,5 @@
 class CommentsController < ApplicationController
-  def index
-    @comments = @product.comments.paginate(:page => params[:page], :per_page => 5)
-  end
+
 
   def create
     @product = Product.find(params[:product_id])
