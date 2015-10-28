0b0b3edba50b1787d3ca5e8924f848ca3158836c
agustinchavez <chavez13@illinois.edu>
2015-10-28 14:37:48 -0500

    Revert "use Ajax to create comments remotely"
    This reverts commit e09a12c7ad1708c221d8e65f38c65985989a19a1.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,14 +1,10 @@
-function updateStars(){
-  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-  $('.rated').raty({ path: '/assets',
-    readOnly: true,
-    score: function() {
-      return $(this).attr('data-score');
-    }
-  });
-};
-
 $(document).on('ready page:load', function(){
-  updateStars();
-  $('.img-zoom').elevateZoom();
+   $('.rating').raty( { path: '/assets' , scoreName: 'comment[rating]'});
+   $('.rated').raty({ path: '/assets',
+      readOnly: true,
+      score: function() {
+        return $(this).attr('data-score');
+      }
+    });
+   $('.img-zoom').elevateZoom();
 });
