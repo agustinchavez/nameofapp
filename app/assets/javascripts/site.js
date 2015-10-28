e09a12c7ad1708c221d8e65f38c65985989a19a1
agustinchavez <chavez13@illinois.edu>
2015-10-28 10:50:57 -0500

    use Ajax to create comments remotely

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,10 +1,14 @@
+function updateStars(){
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
+};
+
 $(document).on('ready page:load', function(){
-   $('.rating').raty( { path: '/assets' , scoreName: 'comment[rating]'});
-   $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
-   $('.img-zoom').elevateZoom();
+  updateStars();
+  $('.img-zoom').elevateZoom();
 });
