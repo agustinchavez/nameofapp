fa64ebc84dc48de6ad8d662d078ae4acbee940d9
agustinchavez <chavez13@illinois.edu>
2015-10-29 15:31:23 -0500

    fied payments controller and added redirect page

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,13 +2,14 @@ class PaymentsController < ApplicationController
   def create
   token = params[:stripeToken]
   @product = Product.find(params[:product_id])
+  @user = current_user
   # Create the charge on Stripe's servers - this will charge the user's card
   begin
     charge = Stripe::Charge.create(
-      :amount => @product.price,
+      :amount => (@product.price).to_i * 100,
       :currency => "usd",
       :source => token,
-      :description => @product.description
+      :description => params[:stripeEmail]
     )
 
     if charge.paid
@@ -25,6 +26,6 @@ class PaymentsController < ApplicationController
         err = body[:error]
         flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
       end
-      redirect_to product_path(product)
+      redirect_to payments_path(@payments)
     end
 end
